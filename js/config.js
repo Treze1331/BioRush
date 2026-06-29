49af2a68ee0d0cbd853615ace3187a377f97ebcf
Treze1331 <murilo14242938@gmail.com>
2026-06-29 12:13:51 -0300

    teste16

--- js/config.js
+++ js/config.js
@@ -1,6 +1,5 @@
-export const SUPABASE_URL = "https://aurmevbsntoatjvhntbh.supabase.co";
-export const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1cm1ldmJzbnRvYXRqdmhudGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0NTQ1NjUsImV4cCI6MjA5ODAzMDU2NX0.oaWwPzz0KZLA1X_2-FWyiCwXTpcg2_goa_ulYWFAZC4";
+export const NEXT_PUBLIC_SUPABASE_URL="https://aurmevbsntoatjvhntbh.supabase.co"
+export const NEXT_PUBLIC_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1cm1ldmJzbnRvYXRqdmhudGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0NTQ1NjUsImV4cCI6MjA5ODAzMDU2NX0.oaWwPzz0KZLA1X_2-FWyiCwXTpcg2_goa_ulYWFAZC4"
 
 export const TOTAL_QUESTIONS = 10;
 export const FEEDBACK_DELAY = 2500;
